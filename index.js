8614238eb24105bc4bebaf8adf2755683eef9d17
Tobiloba <tobiadewole565@gmail.com>
2025-06-23 21:36:12 +0100

    quick update

--- index.js
+++ index.js
@@ -3,10 +3,8 @@ const movieContainer = document.querySelector("main")
 // console.log(movieContainer)
 const api_key= "716d62f1"
 const movieInput = document.getElementById("movie-input")
-const movies = []
-const fullMovies = []
 
-const movie_title = "Blade Runner"
+
 function generateMovie(event){
         event.preventDefault()
         fetch(`https://www.omdbapi.com/?apiKey=${api_key}&s=${movieInput.value}`)
@@ -27,13 +25,13 @@ function generateMovie(event){
                 <img src=${movieList.Poster} class="movie-img">
                 <div class="movie-info">
                     <div class="movie-des">
-                      <h3>${movieList.Title}</h3>
-                      <p class="movie-duration"><i class="fa-solid fa-star"></i>${data.imdbRating}</p>
+                      <h3 class="movie-title">${data.Title}</h3>
+                      <p class="movie-rating"><i class="fa-solid fa-star"></i>${data.imdbRating}</p>
                     </div>
                     <div class="movie-des">
                       <p class="movie-duration">${data.Runtime}</p>
                       <p class="movie-genre">${data.Genre}</p>
-                      <p class="parent"><i class="fa-solid fa-circle-plus"></i>Watchlist</p>
+                      <p class=movie-watch"><i class="fa-solid fa-circle-plus"></i>Watchlist</p>
                     </div>             
                     <p class="movie-plot">${data.Plot}</p>
                 </div>
@@ -41,28 +39,34 @@ function generateMovie(event){
               </div>
           `
           
-          movieContainer.addEventListener("click",(event)=>{
-            movieContainer.style.cursor="pointer"
-               target = event.target
+        
          
-                document.getElementById("watchlist-page").innerHTML = target.innerHTML
-            
-          })
-           console.log(movieContainer)
             const movieAdd = document.querySelectorAll(".fa-circle-plus")
             
             movieAdd.forEach((baby)=> {
               baby.addEventListener('click',(event)=>{
-                let connect =baby.closest('.movie-content')
-                console.log(connect.childNodes[1])
-                
-                let movieWatchlist = document.querySelector(".movie-watchlist")
+                const connect =baby.closest('.movie-content')
+                console
+                const movie = {
+                  poster: connect.querySelector(".movie-img").src,
+                  title: connect.querySelector(".movie-title").textContent,
+                  rating: connect.querySelector(".movie-rating").textContent,
+                  runtime: connect.querySelector(".movie-duration").textContent,
+                  genre: connect.querySelector(".movie-genre").textContent,
+                  plot: connect.querySelector(".movie-plot").textContent
+                }
 
-                = connect.childNodes[1]
+                //Storing data and converting the object into a string
+                localStorage.setItem("selectedMovie",JSON.stringify(movie))
+                window.location.href="watchlist.html"
+                console.log(movie.poster)
+
+                
               })
             })
             
              
+
             //  for(let baby of movieAdd){
             //   baby.addEventListener("click",function(event){
             //     console.log("Baby,I am ready,let go right now")
@@ -73,6 +77,8 @@ function generateMovie(event){
                 
             //   })
             //  }
+
+           
           })
           }
         
@@ -80,22 +86,12 @@ function generateMovie(event){
         })
           
 
-        
-            
-            
-          
-          
-       
-        
-
-      
-    
-
   const container = document.querySelector(".container")
   container.style.visibility = "hidden"
   const main = document.querySelector("main")
   main.style.margin = "0"
    
 }
+
 
 
